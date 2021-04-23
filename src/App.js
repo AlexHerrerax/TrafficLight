ec25f87fd9dbe817c4d7f81a839665da337f4cdf
Alex <axherrerax@gmail.com>
2021-04-23 03:45:06 -0400

    Se corrigieron detalles menores

--- src/App.js
+++ src/App.js
@@ -1,54 +1,33 @@
 import React, { useState } from 'react';
-
 import './App.css';
 
 function App() {
-
-
   const [light, changeLight] = useState({
     lightRed:"red figure",
     lightYellow:"yellow figure",
     lightGreen:"green figure"
   })
 
-  
 function changeRed(){
   changeLight({
     lightRed:"lightRed figure",
     lightYellow:"yellow figure",
     lightGreen:"green figure"
-
-    
-  })
-
-}
+})}
 
 function changeYellow(){
   changeLight({
     lightRed:"red figure",
     lightYellow:"lightYellow figure",
     lightGreen:"green figure"
-
-    
-  })
-
-}
+ })}
 
 function changeGreen(){
   changeLight({
     lightRed:"red figure",
     lightYellow:"yellow figure",
     lightGreen:"lightGreen figure"
-
-    
-  })
-
-}
-
-
-  
-
-
+  })}
 
   return (
 <div className="content">
